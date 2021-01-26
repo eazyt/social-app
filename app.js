823a30f4db8746201f3519d450324db7730798d6
eazyt <ramohlalen@gmail.com>
2021-01-26 17:09:25 +0200

    views folder

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.urlencoded({
 // use ejs-locals for all ejs templates:
 app.engine('ejs', engine);
 
-app.set('views', __dirname + '/views');
+// app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs'); // so you can render('index')
 
 // render 'index' into 'boilerplate':
