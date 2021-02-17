dfc6cae7a7060ebcce84ca2f6051209152d4daab
eazyt <ramohlalen@gmail.com>
2021-02-17 01:17:32 -0800

    userSchema

---
+++ models/UserSchema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: {
+    type: String,
+    default: "/images/profilePic.png"
+  }
+
+})
+
+const User = mongoose.model("User", UserSchema)
+
+module.exports = User;
