7e4d89e9e70c9d893d922e42d2158a03a5126c7d
eazyt <ramohlalen@gmail.com>
2021-02-17 00:46:24 -0800

    body-parser

--- routes/signUpRoute.js
+++ routes/signUpRoute.js
@@ -2,8 +2,24 @@ const router = require('express').Router();
 const middleware = require('../middleware')
 
 router.get('/signup',  (req, res, next) => {
-  res.render('accounts/signup')
-
+  res.status(200).render('accounts/signup')
+  
 });
+
+router.post("/signup", (req, res, next)=>{
+  let firstName = req.body.firstName;
+  let lastName = req.body.lastName;
+  let username = req.body.username;
+  let email = req.body.email;
+  let password = req.body.password;
+  
+  if (firstName && lastName && username && email && password) {
+    
+  } else {
+    
+    res.status(200).render('accounts/signup')
+  }
+
+})
 
 module.exports = router
